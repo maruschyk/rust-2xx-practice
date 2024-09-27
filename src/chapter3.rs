e7767fc9d47a5c314a572aef5b8db12aaf25d014
maruschyk <a.maruschyk@e-u.edu.ua>
2024-09-27 10:04:03 +0300

    Initial commit

---
+++ src/chapter3.rs
@@ -0,0 +1,9 @@
+/// https://practice.course.rs/variables.html
+#[test]
+fn test31() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5);
+    println!("Success!");
+}
